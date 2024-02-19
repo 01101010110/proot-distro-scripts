63aeb7842c34cb3859f5939a88fb690110c00a2b
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-02-19 07:56:03 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ yes | pkg install x11-repo
 yes | pkg update
 yes | pkg uninstall dbus
 yes | pkg install wget dbus proot-distro pulseaudio virglrenderer-android
-yes | pkg install pavucontrol-qt firefox xwayland
+yes | pkg install pavucontrol-qt firefox xwayland xfce4
 yes | proot-distro install debian
 
 # Setup proot
@@ -48,6 +48,10 @@ kill -9 $(pgrep -f "termux.x11") 2>/dev/null
 wget https://github.com/01101010110/proot-distro-scripts/raw/main/termux-x11.deb
 dpkg -i --force-depends termux-x11.deb
 echo "allow-external-apps = true" >> ~/.termux/termux.properties
-termux-x11 :1
-proot-distro login debian --shared-tmp 
-export DISPLAY=:1
+
+# Launch Termux X11 main activity
+am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
+sleep 1
+
+proot-distro login debian --shared-tmp -- /bin/bash -c  'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && -c "env DISPLAY=:0 startxfce4"'
+

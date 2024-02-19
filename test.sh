a53b7ef951b2cad589fdea38286faf9c8c98c571
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-02-19 05:44:22 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ termux-change-repo
 yes | pkg install x11-repo
 yes | pkg update
 yes | pkg uninstall dbus
-yes | pkg install wget dbus proot-distro pulseaudio git virglrenderer-android xfce4 xfce4-goodies pavucontrol-qt jq nala wmctrl firefox 
+yes | pkg install wget dbus proot-distro pulseaudio git virglrenderer-android xfce4 xfce4-goodies pavucontrol-qt jq nala wmctrl firefox xwayland
 yes | proot-distro install debian
 
 # Setup proot
@@ -26,24 +26,6 @@ proot-distro login debian --shared-tmp -- env DISPLAY=:1 useradd -m -g users -G 
 chmod u+rw $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/etc/sudoers
 echo "$username ALL=(ALL) NOPASSWD:ALL" | tee -a $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/etc/sudoers > /dev/null
 chmod u-w $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/etc/sudoers
-
-# Set proot DISPLAY
-echo "export DISPLAY=:1" >> $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/home/$username/.bashrc
-
-# Set proot aliases
-echo "
-alias virgl='GALLIUM_DRIVER=virpipe '
-alias apt='sudo nala '
-" >> $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/home/$username/.bashrc
-
-# Create .bashrc
-cp $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/etc/skel/.bashrc $HOME/.bashrc
-
-# Set Termux aliases
-echo "
-alias debian='termux-x11 :1 && proot-distro login debian --shared-tmp && export DISPLAY=:1'
-alias apt='pkg upgrade -y && nala \$@'
-" >> $HOME/.bashrc
 
 # Enable Sound
 echo "
@@ -62,8 +44,6 @@ chmod +x $HOME/Desktop/firefox.desktop
 wget https://github.com/01101010110/proot-distro-scripts/raw/main/termux-x11.deb
 dpkg -i termux-x11.deb
 echo "allow-external-apps = true" >> ~/.termux/termux.properties
-
-# Apply changes
-source ~/.bashrc
-
-debian
+termux-x11 :1
+proot-distro login debian --shared-tmp 
+export DISPLAY=:1

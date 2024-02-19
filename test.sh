4a03b7ae3a46f0a653944dcd1ea0224151f58411
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-02-19 03:42:28 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -14,37 +14,37 @@ yes | pkg install wget dbus proot-distro pulseaudio git virglrenderer-android xf
 yes | proot-distro install debian
 
 # Setup proot
-proot-distro login debian --shared-tmp -- env DISPLAY=:1 apt update
+yes | proot-distro login debian --shared-tmp -- env DISPLAY=:1 apt update
 yes | proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 apt upgrade
 
-#Create user
+# Create user
 proot-distro login debian -- env DISPLAY=:1 groupadd storage
 proot-distro login debian -- env DISPLAY=:1 groupadd wheel
 proot-distro login debian -- env DISPLAY=:1 useradd -m -g users -G wheel,audio,video,storage -s /bin/bash "$username"
 
-#Add user to sudoers
+# Add user to sudoers
 chmod u+rw $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/etc/sudoers
 echo "$username ALL=(ALL) NOPASSWD:ALL" | tee -a $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/etc/sudoers > /dev/null
-chmod u-w  $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/etc/sudoers
+chmod u-w $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/etc/sudoers
 
-#Set proot DISPLAY
+# Set proot DISPLAY
 echo "export DISPLAY=:1" >> $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/home/$username/.bashrc
 
-#Set proot aliases
+# Set proot aliases
 echo "
 alias virgl='GALLIUM_DRIVER=virpipe '
 alias apt='sudo nala '
 " >> $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/home/$username/.bashrc
 
-#Set proot timezone
+# Set proot timezone
 timezone=$(getprop persist.sys.timezone)
 proot-distro login debian -- env DISPLAY=:1 rm /etc/localtime
-proot-distro login debian  -- env DISPLAY=:1 cp /usr/share/zoneinfo/$timezone /etc/localtime
+proot-distro login debian -- env DISPLAY=:1 cp /usr/share/zoneinfo/$timezone /etc/localtime
 
-#Create .bashrc
+# Create .bashrc
 cp $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/etc/skel/.bashrc $HOME/.bashrc
 
-#Enable Sound
+# Enable Sound
 echo "
 pulseaudio --start --exit-idle-time=-1
 pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
@@ -53,13 +53,13 @@ pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymou
 echo "
 source .sound" >> .bashrc
 
-#Set aliases
+# Set aliases
 echo "
-alias debian='proot-distro login debian --user $username --shared-tmp'
+alias debian='start_debian_x11'
 alias apt='pkg upgrade -y && nala $@'
 " >> $HOME/.bashrc
 
-#Put Firefox icon on Desktop
+# Put Firefox icon on Desktop
 cp $HOME/../usr/share/applications/firefox.desktop $HOME/Desktop 
 chmod +x $HOME/Desktop/firefox.desktop
 
@@ -67,6 +67,28 @@ chmod +x $HOME/Desktop/firefox.desktop
 wget https://github.com/01101010110/proot-distro-scripts/raw/main/termux-x11.deb
 dpkg -i termux-x11.deb
 echo "allow-external-apps = true" >> ~/.termux/termux.properties
-termux-x11 :1.0
-proot-distro login debian --shared-tmp 
-DISPLAY=:1 xfce4-session
+
+# Function to start Debian with X11
+start_debian_x11() {
+    MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 virgl_test_server_android --angle-gl &> /dev/null 2>&1
+    sleep 1
+    XDG_RUNTIME_DIR=${TMPDIR} termux-x11 :1 &
+    sleep 1
+    am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
+    sleep 1
+    proot-distro login debian --user $username --shared-tmp -- env DISPLAY=:1.0 dbus-launch --exit-with-session xfce4-session &> /dev/null 2>&1
+}
+
+# Add the function to bashrc to ensure it gets loaded
+echo 'start_debian_x11() {
+    MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 virgl_test_server_android --angle-gl &> /dev/null 2>&1
+    sleep 1
+    XDG_RUNTIME_DIR=${TMPDIR} termux-x11 :1 &
+    sleep 1
+    am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
+    sleep 1
+    proot-distro login debian --user $username --shared-tmp -- env DISPLAY=:1.0 dbus-launch --exit-with-session xfce4-session &> /dev/null 2>&1
+}' >> $HOME/.bashrc
+
+# Apply changes
+source $HOME/.bashrc

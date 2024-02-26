abf0ee8a99fc7bdd5c8565f89e550b55a09f0ef3
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-02-26 05:52:25 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,6 +10,14 @@ read -r -p "Select a username: " username </dev/tty
 read -r -s -p "Enter password for $username: " password </dev/tty
 echo # move to a new line
 
+# Set timezone
+echo "Please enter your geographical area (e.g., Europe, America, Asia):"
+read AREA
+echo "Please enter your city or closest major city (e.g., Berlin, New_York, Tokyo):"
+read CITY
+echo "tzdata tzdata/Areas select $AREA" | debconf-set-selections
+echo "tzdata tzdata/Zones/$AREA select $CITY" | debconf-set-selections
+
 termux-setup-storage
 termux-change-repo
 
@@ -66,15 +74,6 @@ sleep 3
 # Launch Termux X11 main activity
 am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
 sleep 1
-
-# Set timezone
-echo "Please enter your geographical area (e.g., Europe, America, Asia):"
-read AREA
-echo "Please enter your city or closest major city (e.g., Berlin, New_York, Tokyo):"
-read CITY
-echo "tzdata tzdata/Areas select $AREA" | debconf-set-selections
-echo "tzdata tzdata/Zones/$AREA select $CITY" | debconf-set-selections
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 apt-get install -y tzdata
 
 # Login in to Environment
 proot-distro login ubuntu --shared-tmp -- /bin/bash -c  "export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\${TMPDIR} && su - $username -c \"env DISPLAY=:0 startxfce4\""

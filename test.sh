beac903ed0eb00066212cdfaa42e5aa34e383313
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-02-26 03:23:53 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -67,6 +67,6 @@ am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
 sleep 1
 
 # Login in to Environment. 
-proot-distro login debian --shared-tmp -- /bin/bash -c  "export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\${TMPDIR} && su - $username -c \"env DISPLAY=:0 startxfce4\""
+proot-distro login ubuntu --shared-tmp -- /bin/bash -c  "export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\${TMPDIR} && su - $username -c \"env DISPLAY=:0 startxfce4\""
 
 exit 0

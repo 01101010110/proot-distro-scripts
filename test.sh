b26a53f92ad44bfce769fbe6094a17e9b420279b
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-02-26 22:52:57 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,9 @@
 # Ensure non-interactive frontend for apt-get
 export DEBIAN_FRONTEND=noninteractive
 
+sleep 3
+
+
 # Preconfigure tzdata
 proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 /bin/bash -c "
 echo 'Etc/UTC' > /etc/timezone
@@ -11,9 +14,11 @@ apt-get update
 apt-get install -y tzdata
 dpkg-reconfigure --frontend noninteractive tzdata
 "
+sleep 3
+
 
 # Install xRDP
-yes | proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 sudo apt install xrdp
+yes | proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 apt install xrdp
 
 # Configure xRDP
 proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 echo "xfce4-session" > /home/$USERNAME/.xsession

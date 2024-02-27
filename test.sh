f0272beb7da320a04b2d4ba2b9d185b46169c4d7
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-02-27 00:47:36 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -66,7 +66,11 @@ proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 echo "tzdata tzdata/Zon
 proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 apt install xrdp -y
 
 # Configure xRDP
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 echo "xfce4-session" > /home/$USERNAME/.xsession
+proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 echo "xfce4-session" > /home/$username/.xsession
+
+# Modify the xRDP start script
+proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 sed -i 's|test -x /etc/X11/Xsession && exec /etc/X11/Xsession|exec startxfce4|' /etc/xrdp/startwm.sh
+proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 sed -i '/exec \/bin\/sh \/etc\/X11\/Xsession/d' /etc/xrdp/startwm.sh
 
 # Stop xRDP service
 proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 service xrdp stop

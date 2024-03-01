3108aaaa83cd72ad77aa1e10ddc79b7d632f9b84
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-03-01 09:41:16 -0600

    Update ubuntu-x11-app.bash

--- ubuntu-x11-app.bash
+++ ubuntu-x11-app.bash
@@ -69,5 +69,6 @@ sleep 1
 echo "alias ubuntu='proot-distro login ubuntu --shared-tmp -- /bin/bash -c \"export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\\\${TMPDIR} && su - \$username -c \\\"env DISPLAY=:0 termux-x11 :0 -xstartup \\\\\\\"dbus-launch --exit-with-session startxfce4\\\\\\\"\"'" >> $HOME/.bashrc
 
 # Login in to Environment
-proot-distro login ubuntu --shared-tmp -- /bin/bash -c "export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\${TMPDIR} && su - \$username -c 'env DISPLAY=:0 termux-x11 :0 -xstartup \"dbus-launch --exit-with-session startxfce4\"'"
+proot-distro login ubuntu --shared-tmp && su - \$username -c termux-x11 :0 -xstartup "dbus-launch --exit-with-session xfce4-session"
+
 exit 0

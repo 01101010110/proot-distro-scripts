8f5c0928f518fa0e836bffaa9881cfad1cee7f13
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-03-01 09:49:22 -0600

    Update ubuntu-x11-app.bash

--- ubuntu-x11-app.bash
+++ ubuntu-x11-app.bash
@@ -66,9 +66,8 @@ am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
 sleep 1
 
 # Set an alias in Termux to login to proot-distro easier
-echo "alias ubuntu='proot-distro login ubuntu --shared-tmp && su - \$username -c \"termux-x11 :0 -xstartup \\\"dbus-launch --exit-with-session xfce4-session\\\"\"'" >> $HOME/.bashrc
+echo "alias ubuntu='proot-distro login ubuntu --shared-tmp -- /bin/bash -c \"export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\\\${TMPDIR} && su - $username -c \\\"env DISPLAY=:0 startxfce4\\\"\"'" >> $HOME/.bashrc
 
 # Login in to Environment
-proot-distro login ubuntu --shared-tmp && su - \$username -c termux-x11 :0 -xstartup "dbus-launch --exit-with-session xfce4-session"
-
+proot-distro login ubuntu --shared-tmp -- /bin/bash -c "export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\${TMPDIR} && su - $username -c \"env DISPLAY=:0 startxfce4\""
 exit 0

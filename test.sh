06d1233d2920501ce3feeb9d2ba2f8b866e1b463
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-03-02 02:54:05 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,10 +6,10 @@ read -r -p "Select a username: " username </dev/tty
 
 
 # Configure xRDP
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 echo "xfce4-session" > /home/$username/.xsession
+echo "xfce4-session" > /home/$username/.xsession
 
 # Modify the xRDP start script
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 sed -i 's|test -x /etc/X11/Xsession && exec /etc/X11/Xsession|exec startxfce4|' /etc/xrdp/startwm.sh
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 sed -i '/exec \/bin\/sh \/etc\/X11\/Xsession/d' /etc/xrdp/startwm.sh
+sed -i 's|test -x /etc/X11/Xsession && exec /etc/X11/Xsession|exec startxfce4|' /etc/xrdp/startwm.sh
+sed -i '/exec \/bin\/sh \/etc\/X11\/Xsession/d' /etc/xrdp/startwm.sh
 
 

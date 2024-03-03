43bbdb6bbdf3c774472e6460dc5496b8862968a7
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-03-03 02:43:42 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Install xRDP
-apt install xrdp -y
+sudo apt install xrdp -y
 
 # Configure xRDP
-echo "xfce4-session" > /home/$username/.xsession
 sed -i 's|test -x /etc/X11/Xsession && exec /etc/X11/Xsession|exec startxfce4|' /etc/xrdp/startwm.sh
 sed -i '/exec \/bin\/sh \/etc\/X11\/Xsession/d' /etc/xrdp/startwm.sh
 

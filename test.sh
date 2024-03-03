f78f1762ae333f546fc5013fb447ea4b9c084461
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-03-03 02:55:18 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,6 +7,10 @@ sudo apt install xrdp -y
 sed -i 's|test -x /etc/X11/Xsession && exec /etc/X11/Xsession|exec startxfce4|' /etc/xrdp/startwm.sh
 sed -i '/exec \/bin\/sh \/etc\/X11\/Xsession/d' /etc/xrdp/startwm.sh
 
+# Create an alias to restart the xrdp server in the future by typing the wword 'run' 
+echo "alias run='sudo service xrdp stop && sudo service xrdp start'" >> ~/.bashrc
+source ~/.bashrc
+
 # Start the xrdp server
 sudo service xrdp stop
 sudo service xrdp start

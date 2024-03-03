b3ec20e205ba6b55237bc05eb7bdb34b38e60527
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-03-03 11:07:46 -0600

    Update termux-x11-app.sh

--- termux-x11-app.sh
+++ termux-x11-app.sh
@@ -1,40 +1,18 @@
 #!/bin/bash
 
-# Set Username
-read -r -p "Select a username: " username </dev/tty
-
-# Set Password
-read -r -s -p "Enter password for $username: " password </dev/tty
-echo # move to a new line
-
+# To avoid a repo bug
 termux-change-repo
 
-# Update and install required packages
+# Install the x11-repo and update all packages
 yes | pkg install x11-repo
 yes | pkg update
-yes | pkg uninstall dbus
-pkg install wget dbus proot-distro pulseaudio virglrenderer-android -y
-pkg install pavucontrol-qt firefox xfce4 -y
-yes | proot-distro install ubuntu
 
-# Setup proot
-yes | proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 apt update
-yes | proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 apt upgrade
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 apt install sudo -y
+# Grant storage access - (cannot be ran prior to installing x11-repo)
+termux-setup-storage
 
-# Create user
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 groupadd storage
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 groupadd wheel
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 useradd -m -g users -G wheel,audio,video,storage -s /bin/bash "$username"
-
-# Set user password
-echo "$username:$password" | proot-distro login ubuntu --shared-tmp -- chpasswd
-
-# Add user to sudoers
-chmod u+rw $HOME/../usr/var/lib/proot-distro/installed-rootfs/ubuntu/etc/sudoers
-echo "$username ALL=(ALL) ALL" | tee -a $HOME/../usr/var/lib/proot-distro/installed-rootfs/ubuntu/etc/sudoers > /dev/null
-chmod u-w $HOME/../usr/var/lib/proot-distro/installed-rootfs/ubuntu/etc/sudoers
-
+# Install hardware acceleration, sound, and firefox
+pkg install dbus pulseaudio virglrenderer-android -y
+pkg install pavucontrol-qt firefox -y
 # Enable Sound
 echo "
 pulseaudio --start --exit-idle-time=-1
@@ -66,9 +44,9 @@ am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
 sleep 1
 
 # Set an alias in Termux to login to proot-distro easier
-echo "alias ubuntu='am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1 && sleep 1 && GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 proot-distro login ubuntu --shared-tmp -- /bin/bash -c \"export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\\\\\${TMPDIR} && su - $username -c \\\"sh -c \\\\\\\"termux-x11 :0 -xstartup \\\\\\\\\\\\\\\"dbus-launch --exit-with-session xfce4-session\\\\\\\\\\\\\\\" && env DISPLAY=:0 startxfce4\\\\\\\"\\\"\"'" >> $HOME/.bashrc
+echo "alias termux='am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1 && sleep 1 && -- /bin/bash -c \"export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\\\\\${TMPDIR} && -c \\\"sh -c \\\\\\\"termux-x11 :0 -xstartup \\\\\\\\\\\\\\\"dbus-launch --exit-with-session xfce4-session\\\\\\\\\\\\\\\" && startxfce4\\\\\\\"\\\"\"'" >> $HOME/.bashrc
 
 # Login to Environment
-GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 proot-distro login ubuntu --shared-tmp -- /bin/bash -c "export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\${TMPDIR} && su - $username -c \"termux-x11 :0 -xstartup \\\"dbus-launch --exit-with-session xfce4-session\\\" && env DISPLAY=:0 startxfce4\""
+-- /bin/bash -c "export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\${TMPDIR} && -c \"termux-x11 :0 -xstartup \\\"dbus-launch --exit-with-session xfce4-session\\\" && startxfce4\""
 
 exit 0

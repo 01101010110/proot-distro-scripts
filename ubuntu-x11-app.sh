d7d682d5ee15aa661fa7ecde5a772f5da4430397
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-03-03 08:47:01 -0600

    Update ubuntu-x11-app.sh

--- ubuntu-x11-app.sh
+++ ubuntu-x11-app.sh
@@ -7,20 +7,39 @@ read -r -p "Select a username: " username </dev/tty
 read -r -s -p "Enter password for $username: " password </dev/tty
 echo # move to a new line
 
+# To avoid a repo bug
 termux-change-repo
 
-# Update and install required packages
+# Install the x11-repo and update all packages
 yes | pkg install x11-repo
 yes | pkg update
-yes | pkg uninstall dbus
-pkg install wget dbus proot-distro pulseaudio virglrenderer-android -y
+
+# Grant storage access - (cannot be ran prior to installing x11-repo)
+termux-setup-storage
+
+# Install hardware acceleration, proot-distro, sound, and firefox
+pkg install dbus proot-distro pulseaudio virglrenderer-android -y
 pkg install pavucontrol-qt firefox -y
+
+# Install ubuntu in proot-distro
 yes | proot-distro install ubuntu
 
-# Setup proot
+# Update proot-distro
 yes | proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 apt update
 yes | proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 apt upgrade
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 apt install sudo xfce4 dbus-x11 -y
+
+#Install proot-distro packages (sudo, gui, terminal)
+proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 apt install sudo xfce4 xfce4-terminal dbus-x11 -y
+
+# Removes glitchy xterminal
+proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 apt remove xterm -y
+
+# Set xfce4-terminal as the default terminal
+proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/xfce4-terminal 50
+proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 update-alternatives --set x-terminal-emulator /usr/bin/xfce4-terminal
+
+# Removes ubuntu Sensible Web Browser
+proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 apt remove sensible-utils -y
 
 # Create user
 proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 groupadd storage
@@ -56,7 +75,9 @@ pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth
 
 # Prepare termux-x11 session
 export XDG_RUNTIME_DIR=${TMPDIR}
-termux-x11 :0 >/dev/null &
+
+# Set Display to :1 since Ubuntu runs on :0
+termux-x11 :1 >/dev/null &
 
 # Wait a bit until termux-x11 gets started.
 sleep 3
@@ -66,9 +87,10 @@ am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
 sleep 1
 
 # Set an alias in Termux to login to proot-distro easier
-echo "alias ubuntu='am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1 && sleep 1 && GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 proot-distro login ubuntu --shared-tmp -- /bin/bash -c \"export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\\\\\${TMPDIR} && su - $username -c \\\"sh -c \\\\\\\"termux-x11 :0 -xstartup \\\\\\\\\\\\\\\"dbus-launch --exit-with-session xfce4-session\\\\\\\\\\\\\\\" && env DISPLAY=:0 startxfce4\\\\\\\"\\\"\"'" >> $HOME/.bashrc
+echo "alias ubuntu='am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1 && sleep 1 && GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 proot-distro login ubuntu --shared-tmp -- /bin/bash -c \"export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\\\\\${TMPDIR} && su - $username -c \\\"sh -c \\\\\\\"termux-x11 :1 -xstartup \\\\\\\\\\\\\\\"dbus-launch --exit-with-session xfce4-session\\\\\\\\\\\\\\\" && env DISPLAY=:1 startxfce4\\\\\\\"\\\"\"'" >> $HOME/.bashrc
+source ~/.bashrc
 
 # Login to Environment
-GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 proot-distro login ubuntu --shared-tmp -- /bin/bash -c "export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\${TMPDIR} && su - $username -c \"termux-x11 :0 -xstartup \\\"dbus-launch --exit-with-session xfce4-session\\\" && env DISPLAY=:0 startxfce4\""
+GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 proot-distro login ubuntu --shared-tmp -- /bin/bash -c "export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\${TMPDIR} && su - $username -c \"termux-x11 :1 -xstartup \\\"dbus-launch --exit-with-session xfce4-session\\\" && env DISPLAY=:1 startxfce4\""
 
 exit 0

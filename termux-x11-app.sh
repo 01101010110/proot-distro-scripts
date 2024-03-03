411ee08d53a5dbba3a8668ff1d1b2024b9c16d87
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-03-03 11:33:31 -0600

    Update termux-x11-app.sh

--- termux-x11-app.sh
+++ termux-x11-app.sh
@@ -44,11 +44,10 @@ sleep 3
 am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
 sleep 1
 
-# Set an alias in Termux to login to proot-distro easier
-echo "alias termux='am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1 && sleep 1 && -- /bin/bash -c \"export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\\\\\${TMPDIR} && -c \\\"sh -c \\\\\\\"termux-x11 :3 -xstartup \\\\\\\\\\\\\\\"dbus-launch --exit-with-session xfce4-session\\\\\\\\\\\\\\\" && startxfce4\\\\\\\"\\\"\"'" >> $HOME/.bashrc
+echo "alias termux='am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity >/dev/null 2>&1 && sleep 1 && termux-x11 :3 -xstartup \"dbus-launch --exit-with-session xfce4-session\" && startxfce4'" >> $HOME/.bashrc
 source ~/.bashrc
 
 # Login to Environment
--- /bin/bash -c "export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\${TMPDIR} && -c \"termux-x11 :3 -xstartup \\\"dbus-launch --exit-with-session xfce4-session\\\" && startxfce4\""
+termux-x11 :3 -xstartup \"dbus-launch --exit-with-session xfce4-session\" && startxfce4
 
 exit 0

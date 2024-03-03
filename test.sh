83aa4e9f7aca03892e778e1860e4d01149ea1995
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-03-03 03:14:30 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,9 +8,23 @@ sed -i 's|test -x /etc/X11/Xsession && exec /etc/X11/Xsession|exec startxfce4|' 
 sed -i '/exec \/bin\/sh \/etc\/X11\/Xsession/d' /etc/xrdp/startwm.sh
 
 # Create an alias to restart the xrdp server in the future by typing the wword 'run' 
-echo "alias run='sudo service xrdp stop && sudo service xrdp start'" >> ~/.bashrc
+#echo "alias run='sudo service xrdp stop && sudo service xrdp start'" >> ~/.bashrc
+echo "alias run='sudo service xrdp stop && sudo service xrdp start && echo "Local IP Address:" && ifconfig | grep "inet " | cut -d" " -f10'" >> ~/.bashrc
 source ~/.bashrc
 
 # Start the xrdp server
 sudo service xrdp stop
 sudo service xrdp start
+
+# Clear the screen
+clear
+
+# Display local ip address 
+ifconfig | grep 'inet' | cut -d' ' -f10
+
+# Show user completion message
+echo "====================="
+echo "Installation complete
+echo "====================="
+echo
+echo "Use your local ip address provided above (usually begins with '192.168') to connect remotely"

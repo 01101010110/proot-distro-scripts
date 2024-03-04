dd145b8358cbba359136bdb90f5bdc7c850d4704
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-03-04 00:17:18 -0600

    Update setup-xrdp.sh

--- setup-xrdp.sh
+++ setup-xrdp.sh
@@ -1,28 +1,56 @@
 #!/bin/bash
 
-# Install xRDP
-sudo apt install xrdp -y
+# Ask the user about the environment
+echo "Is this installation for a Termux environment? (yes/no)"
+read IS_TERMUX
 
-# Configure xRDP
-sed -i 's|test -x /etc/X11/Xsession && exec /etc/X11/Xsession|exec startxfce4|' /etc/xrdp/startwm.sh
-sed -i '/exec \/bin\/sh \/etc\/X11\/Xsession/d' /etc/xrdp/startwm.sh
+if [ "$IS_TERMUX" = "yes" ]; then
+    # Termux environment setup
+    echo "Setting up xRDP for Termux..."
 
-# Create an alias to restart the xrdp server in the future by typing the wword 'run' 
-echo "alias run='sudo GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 service xrdp stop && sudo GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 service xrdp start && ifconfig | grep 'inet''" >> ~/.bashrc
-source ~/.bashrc
+    # Install xRDP
+    pkg install xrdp -y
 
-# Start the xrdp server
-sudo GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 service xrdp stop
-sudo GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 service xrdp start
+    # Configure xRDP
+    sed -i 's|test -x /etc/X11/Xsession && exec /etc/X11/Xsession|exec startxfce4|' /etc/xrdp/startwm.sh
+    sed -i '/exec \/bin\/sh \/etc\/X11\/Xsession/d' /etc/xrdp/startwm.sh
+
+    # Create an alias to restart the xrdp server in the future by typing the word 'run'
+    echo "alias run='service xrdp stop && service xrdp start && ifconfig | grep inet'" >> ~/.bashrc
+    source ~/.bashrc
+
+    # Start the xrdp server
+    service xrdp stop
+    service xrdp start
+
+else
+    # Linux environment setup
+    echo "Setting up xRDP for Linux..."
+
+    # Install xRDP
+    sudo apt install xrdp -y
+
+    # Configure xRDP
+    sed -i 's|test -x /etc/X11/Xsession && exec /etc/X11/Xsession|exec startxfce4|' /etc/xrdp/startwm.sh
+    sed -i '/exec \/bin\/sh \/etc\/X11\/Xsession/d' /etc/xrdp/startwm.sh
+
+    # Create an alias to restart the xrdp server in the future by typing the word 'run'
+    echo "alias run='sudo GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 service xrdp stop && service xrdp start && ifconfig | grep inet'" >> ~/.bashrc
+    source ~/.bashrc
+
+    # Start the xrdp server
+    sudo service xrdp stop
+    sudo service xrdp start
+fi
 
 # Clear the screen
 clear
 
-# Display local ip address 
+# Display local IP address
 ifconfig | grep 'inet' | cut -d' ' -f10
 
 # Show user completion message
 echo "====================="
 echo "Installation complete"
 echo "====================="
-echo "Use your local ip address provided above (usually begins with '192.168') to connect remotely"
+echo "Use your local IP address provided above (usually begins with '192.168') to connect remotely."

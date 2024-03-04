076d25db74103fa1d3c4f787392d2238849c1573
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-03-04 06:19:24 -0600

    Update setup-xrdp.sh

--- setup-xrdp.sh
+++ setup-xrdp.sh
@@ -5,8 +5,9 @@ clear
 
 # Ask the user about the environment
 echo "Is the xrdp installation for a Termux environment?"
-echo "Say no if you are installing xrdp for Linux environments like, Ubuntu, Debian, etc."
-echo "Enter (yes/no)"
+echo "Say no if you are installing xrdp for Linux environments." 
+echo "(Linux environments like, Ubuntu, Debian, etc.)"
+echo "Enter yes/no"
 read IS_TERMUX
 
 # Set alias called 'inet' to pull local ip address

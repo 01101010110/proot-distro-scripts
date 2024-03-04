1c448e92a7e3686f4b247d9ed527173802945068
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-03-04 03:56:29 -0600

    Update setup-xrdp.sh

--- setup-xrdp.sh
+++ setup-xrdp.sh
@@ -40,11 +40,11 @@ else
     sed -i 's|test -x /etc/X11/Xsession && exec /etc/X11/Xsession|exec startxfce4|' /etc/xrdp/startwm.sh
     sed -i '/exec \/bin\/sh \/etc\/X11\/Xsession/d' /etc/xrdp/startwm.sh
 
-    # Create an alias to restart the xrdp server in the future by typing the word 'run'
-    echo "alias run='sudo GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 service xrdp stop && service xrdp start && ifconfig | grep inet'" >> ~/.bashrc
+    # Create an alias to restart the xrdp server in the future by typing 'xrdpstart', this is inverse to the termux alias as to not cause conflicts
+    echo "alias xrdpstart='sudo GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 service xrdp stop && service xrdp start && ifconfig | grep inet'" >> ~/.bashrc
 
-    # Create an alias to stop the xrdp server by typing stopxrdp    
-    echo "alias stopxrdp='sudo GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 service xrdp stop'" >> ~/.bashrc
+    # Create an alias to stop the xrdp server by typing xrdpstop   
+    echo "alias xrdpstop='sudo GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 service xrdp stop'" >> ~/.bashrc
 
     # Load the aliases
     source ~/.bashrc

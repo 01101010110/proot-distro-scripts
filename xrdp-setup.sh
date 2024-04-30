49dee128364a16f088dbee51087b9bb8da10bbb7
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-04-30 05:42:22 -0500

    Update xrdp-setup.sh

--- xrdp-setup.sh
+++ xrdp-setup.sh
@@ -12,7 +12,7 @@ proot-distro login debian --shared-tmp -- env DISPLAY=:1 echo "alias xrdpstart='
 proot-distro login debian --shared-tmp -- env DISPLAY=:1 echo "alias xrdpstop='sudo service xrdp stop'" >> ~/.bashrc
 
 # Set alias called 'inet' to pull local ip address
-proot-distro login debian --shared-tmp -- env DISPLAY=:1 echo "alias inet='ifconfig | grep inet | awk "{print \$2}"'" >> ~/.bashrc && source ~/.bashrc
+echo \"alias inet='ifconfig | grep inet | awk \\"'{print \\\$2}'\\"'\" >> ~/.bashrc && source ~/.bashrc
 
 # Load the aliases, clear the screen, start xrdp. then display local ip address to connect to
 proot-distro login debian --shared-tmp -- env DISPLAY=:1 clear && xrdpstart && inet

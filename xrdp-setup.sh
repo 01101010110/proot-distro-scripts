7a7dc3e139a41f090f8eab919ee0ce4e840f78c3
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-04-30 05:30:17 -0500

    Update xrdp-setup.sh

--- xrdp-setup.sh
+++ xrdp-setup.sh
@@ -1,20 +1,20 @@
 # Install xRDP
-sudo apt install xrdp -y
+proot-distro login debian --shared-tmp -- env DISPLAY=:1 apt install xrdp -y
 
 # Configure xRDP
-sed -i 's|test -x /etc/X11/Xsession && exec /etc/X11/Xsession|exec startxfce4|' /etc/xrdp/startwm.sh
-sed -i '/exec \/bin\/sh \/etc\/X11\/Xsession/d' /etc/xrdp/startwm.sh
+proot-distro login debian --shared-tmp -- env DISPLAY=:1 sed -i 's|test -x /etc/X11/Xsession && exec /etc/X11/Xsession|exec startxfce4|' /etc/xrdp/startwm.sh
+proot-distro login debian --shared-tmp -- env DISPLAY=:1 sed -i '/exec \/bin\/sh \/etc\/X11\/Xsession/d' /etc/xrdp/startwm.sh
 
 # Create an alias to restart the xrdp server in the future by typing 'xrdpstart', this is inverse to the termux alias as to not cause conflicts
-echo "alias xrdpstart='sudo service xrdp stop && sudo service xrdp start && inet'" >> ~/.bashrc
+proot-distro login debian --shared-tmp -- env DISPLAY=:1 echo "alias xrdpstart='sudo service xrdp stop && sudo service xrdp start && inet'" >> ~/.bashrc
 
 # Create an alias to stop the xrdp server by typing xrdpstop   
-echo "alias xrdpstop='sudo service xrdp stop'" >> ~/.bashrc
+proot-distro login debian --shared-tmp -- env DISPLAY=:1 echo "alias xrdpstop='sudo service xrdp stop'" >> ~/.bashrc
 
 # Set alias called 'inet' to pull local ip address
-echo "alias inet='ifconfig | grep inet | awk "{print \$2}"'" >> ~/.bashrc
+proot-distro login debian --shared-tmp -- env DISPLAY=:1 echo "alias inet='ifconfig | grep inet | awk "{print \$2}"'" >> ~/.bashrc
 
 # Load the aliases, clear the screen, start xrdp. then display local ip address to connect to
-source ~/.bashrc && clear && xrdpstart && inet
+proot-distro login debian --shared-tmp -- env DISPLAY=:1 source ~/.bashrc && clear && xrdpstart && inet
 
 

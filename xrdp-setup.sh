12b7450827a0d05245c7bb68917caeff4bd999ba
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-04-30 04:55:40 -0500

    Update xrdp-setup.sh

--- xrdp-setup.sh
+++ xrdp-setup.sh
@@ -1,6 +1,9 @@
 # Install xRDP
 sudo apt install xrdp -y
 
+# Set alias called 'inet' to pull local ip address
+alias inet='ifconfig | grep inet | awk "{print \$2}"'
+
 # Configure xRDP
 sed -i 's|test -x /etc/X11/Xsession && exec /etc/X11/Xsession|exec startxfce4|' /etc/xrdp/startwm.sh
 sed -i '/exec \/bin\/sh \/etc\/X11\/Xsession/d' /etc/xrdp/startwm.sh

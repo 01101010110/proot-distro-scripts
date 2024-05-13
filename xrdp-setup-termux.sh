3a7958c6806cbe3fc1e44450acc950b43624fde1
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-05-13 04:58:24 -0500

    Update xrdp-setup-termux.sh

--- xrdp-setup-termux.sh
+++ xrdp-setup-termux.sh
@@ -1,17 +1,17 @@
+#!/bin/bash
+
 # Install xRDP
 pkg install xrdp -y
 
 # Configure xRDP
-sed -i 's/port=-1/port=5901/' ../usr/etc/xrdp/xrdp.ini &&
+sed -i 's/port=-1/port=5901/' $PREFIX/etc/xrdp/xrdp.ini
 
-# Create an Alias called 'stopxrdp' to stop the xRDP server and load the alias when completed
-alias stopxrdp='xrdp -k && vncserver -kill :1'
+# Write aliases to the shell configuration file
+echo "alias stopxrdp='xrdp -k && vncserver -kill :1'" >> ~/.bashrc
+echo "alias startxrdp='stopxrdp && xrdp && vncserver -xstartup /usr/bin/startxfce4 -listen tcp :1 && ifconfig'" >> ~/.bashrc
 
-# Create an Alias called 'startxrdp' to start the xRDP server quickly and load the alias when completed
-alias startxrdp='stopxrdp && xrdp && vncserver -xstartup /usr/bin/startxfce4 -listen tcp :1 && ifconfig'
+# Load the aliases for the current session
+source ~/.bashrc
 
-# Clear the screen
-clear
-
-# Start the xrdp server and display local ip address
-xrdp && vncserver -xstartup /usr/bin/startxfce4 -listen tcp :1 && ifconfig
+# Let user know installation is complete
+clear && echo "XRDP is installed. To use, type startxrdp and connect to the WLAN0 local IP with your PC."

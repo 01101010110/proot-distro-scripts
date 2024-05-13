056563db7f8af05b89143ad8518dd5778a21044b
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-05-13 05:07:32 -0500

    Update xrdp-setup-termux.sh

--- xrdp-setup-termux.sh
+++ xrdp-setup-termux.sh
@@ -13,5 +13,8 @@ echo "alias startxrdp='stopxrdp && xrdp && vncserver -xstartup /usr/bin/startxfc
 # Load the aliases for the current session
 source ~/.bashrc
 
+# Setup the vnc server and password
+xrdp && vncserver -xstartup /usr/bin/startxfce4 -listen tcp :1 
+
 # Let user know installation is complete
 clear && echo "XRDP is installed. To use, type startxrdp and connect to the WLAN0 local IP with your PC."

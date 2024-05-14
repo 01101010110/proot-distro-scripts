ebe73b4a5fa1887fd1409d57965c9daf624fd6e8
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-05-14 05:09:54 -0500

    Update xrdp-setup-termux.sh

--- xrdp-setup-termux.sh
+++ xrdp-setup-termux.sh
@@ -8,7 +8,7 @@ sed -i 's/port=-1/port=5901/' $PREFIX/etc/xrdp/xrdp.ini
 
 # Write aliases to the shell configuration file
 echo "alias stopxrdp='xrdp -k && vncserver -kill :1'" >> ~/.bashrc
-echo "alias startxrdp='stopxrdp && xrdp && vncserver -xstartup /usr/bin/startxfce4 -listen tcp :1 && ifconfig'" >> ~/.bashrc
+echo "alias startxrdp='xrdp && vncserver -xstartup /usr/bin/startxfce4 -listen tcp :1 && ifconfig'" >> ~/.bashrc
 
 # Load the aliases for the current session
 source ~/.bashrc

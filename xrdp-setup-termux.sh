0d1e6f2a45adc49a6446fc8ffbc5755be1ba0328
01101010110 <61366489+01101010110@users.noreply.github.com>
2024-05-14 06:08:31 -0500

    Update xrdp-setup-termux.sh

--- xrdp-setup-termux.sh
+++ xrdp-setup-termux.sh
@@ -11,14 +11,12 @@ echo -e "#!/bin/bash\nxrdp && vncserver -xstartup /usr/bin/startxfce4 -listen tc
 chmod +x /data/data/com.termux/files/usr/bin/startxrdp
 
 # Write aliases to the shell configuration file
-
-# Set an alias to load termux environment faster
 echo 'alias stopxrdp="xrdp -k && vncserver -kill :1"' >> $HOME/.bashrc
-echo 'alias startxrdp="xrdp && vncserver -xstartup /usr/bin/startxfce4 -listen tcp :1 && ifconfig"' >> $HOME/.bashrc
+#echo 'alias startxrdp="xrdp && vncserver -xstartup /usr/bin/startxfce4 -listen tcp :1 && ifconfig"' >> $HOME/.bashrc
 source ~/.bashrc
 
 # Setup the vnc server and password
 xrdp && vncserver -xstartup /usr/bin/startxfce4 -listen tcp :1 && stopxrdp
 
 # Let user know installation is complete
-clear && echo "XRDP is installed. To use, type startxrdp and connect to the WLAN0 local IP with your PC. You can close xrdp by typing stopxrdp."
+clear && echo "XRDP is installed. To use, type xrdp && ifconfig and connect to the WLAN0 local IP with your PC. You can close xrdp by typing stopxrdp."

88bdf3e8f6ffd9537526f8fd1bfd7685e8bfb016
01101010110 <61366489+01101010110@users.noreply.github.com>
2025-07-14 18:04:45 -0500

    Update xrdp-setup-termux.sh

--- xrdp-setup-termux.sh
+++ xrdp-setup-termux.sh
@@ -2,17 +2,23 @@
 
 # Install xRDP
 pkg install xrdp -y
+pkg install -y xrdp tigervnc xfce4
 
-# Configure xRDP
-sed -i 's/port=-1/port=5901/' $PREFIX/etc/xrdp/xrdp.ini
+# Create VNC startup file
+mkdir -p ~/.vnc
+echo "startxfce4 &" > ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
 
-# Write aliases to the shell configuration file
-echo 'alias stopxrdp="xrdp -k && vncserver -kill :1"' >> $HOME/.bashrc
-echo 'alias startxrdp="xrdp && vncserver -xstartup /usr/bin/startxfce4 -listen tcp :1 && ifconfig"' >> $HOME/.bashrc
+# Configure stopxrdp alias to clean up all processes and locks
+sed -i '/alias stopxrdp/d' ~/.bashrc
+echo 'alias stopxrdp="pkill -9 xrdp; pkill -9 sesman; pkill -9 Xtightvnc; pkill -9 Xvnc; pkill -9 Xorg; pkill -9 vncserver; rm -f ~/.vnc/*:1.pid /data/data/com.termux/files/usr/tmp/.X1-lock /data/data/com.termux/files/usr/tmp/.X11-unix/X1"' >> ~/.bashrc
+
+# Configure startxrdp alias to clean and launch fresh
+sed -i '/alias startxrdp/d' ~/.bashrc
+echo 'alias startxrdp="stopxrdp; sleep 1; vncserver :1 -geometry 1280x720 -localhost no && xrdp"' >> ~/.bashrc
+
+# Reload aliases
 source ~/.bashrc
-
-# Setup the vnc server and password
-xrdp && vncserver -xstartup /usr/bin/startxfce4 -listen tcp :1 && stopxrdp
 
 # Let user know installation is complete
 clear && echo "XRDP is installed. To use, type startxrdp && ifconfig and connect to the WLAN0 local IP with your PC. You can close xrdp by typing stopxrdp."

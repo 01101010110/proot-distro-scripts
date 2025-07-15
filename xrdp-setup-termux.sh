32ced2233d7771d059c71a2aa5da308a7e3c531c
01101010110 <61366489+01101010110@users.noreply.github.com>
2025-07-15 13:31:32 -0500

    Update xrdp-setup-termux.sh

--- xrdp-setup-termux.sh
+++ xrdp-setup-termux.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+# Install required packages
 pkg install x11-repo -y
 pkg update -y
 pkg install xfce4 xfce4-goodies tigervnc xrdp -y
 
+# Fix port in xrdp.ini
 sed -i 's/port=-1/port=5901/' ../usr/etc/xrdp/xrdp.ini
 
+# Create VNC startup files
+mkdir -p ~/.vnc
+echo "#!/data/data/com.termux/files/usr/bin/sh" > ~/.vnc/xstartup
+echo "startxfce4 &" >> ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
+
+# Create .xsession for XRDP
+echo "startxfce4" > ~/.xsession
+chmod +x ~/.xsession
+
+# Start services
+vncserver -geometry 1280x720 :1
 xrdp
-vncserver -xstartup ../usr/bin/startxfce4 -listen tcp :1

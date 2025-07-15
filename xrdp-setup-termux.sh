756afe0800ab79b2c7651ea718f85eaa64b9b56a
01101010110 <61366489+01101010110@users.noreply.github.com>
2025-07-15 13:42:25 -0500

    Update xrdp-setup-termux.sh

--- xrdp-setup-termux.sh
+++ xrdp-setup-termux.sh
@@ -2,7 +2,7 @@
 
 pkg install x11-repo -y
 pkg update -y
-pkg install xfce4 xfce4-goodies xrdp xorg-xrdp -y
+pkg install xfce4 xfce4-goodies tigervnc xrdp -y
 
 cat > ../usr/etc/xrdp/xrdp.ini << 'EOF'
 [Globals]
@@ -11,22 +11,32 @@ fork=true
 port=3389
 
 [xrdp1]
-name=Xorg-Session
-lib=libxorgxrdp.so
+name=VNC-Session
+lib=libvnc.so
 username=ask
 password=ask
 ip=127.0.0.1
-port=-1
+port=5901
 EOF
+
+mkdir -p ~/.vnc
+echo "#!/data/data/com.termux/files/usr/bin/sh" > ~/.vnc/xstartup
+echo "startxfce4 &" >> ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
 
 echo "startxfce4" > ~/.xsession
 chmod +x ~/.xsession
 
 pkill -9 xrdp
 pkill -9 sesman
-rm -f /data/data/com.termux/files/usr/tmp/.X11-unix/X0
-rm -f /data/data/com.termux/files/usr/tmp/.X0-lock
+pkill -f Xtigervnc
+vncserver -kill :1 > /dev/null 2>&1
+
+rm -f ~/.vnc/*:1.pid
+rm -f /data/data/com.termux/files/usr/tmp/.X1-lock
+rm -f /data/data/com.termux/files/usr/tmp/.X11-unix/X1
 
 sleep 2
 
+vncserver -geometry 1280x720 :1
 xrdp

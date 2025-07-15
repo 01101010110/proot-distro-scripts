fbdaadb4a1763815b6e31b318a2684f3340966ad
01101010110 <61366489+01101010110@users.noreply.github.com>
2025-07-15 14:03:06 -0500

    Update xrdp-setup-termux.sh

--- xrdp-setup-termux.sh
+++ xrdp-setup-termux.sh
@@ -4,26 +4,6 @@ pkg install x11-repo -y
 pkg update -y
 pkg install xfce4 xfce4-goodies tigervnc xrdp -y
 
-cat > ../usr/etc/xrdp/xrdp.ini << 'EOF'
-[Globals]
-ini_version=1
-fork=true
-port=3389
-
-[xrdp1]
-name=VNC-Session
-lib=libvnc.so
-username=ask
-password=ask
-ip=127.0.0.1
-port=5901
-EOF
-
-mkdir -p ~/.vnc
-echo "#!/data/data/com.termux/files/usr/bin/sh" > ~/.vnc/xstartup
-echo "startxfce4 &" >> ~/.vnc/xstartup
-chmod +x ~/.vnc/xstartup
-
 echo "startxfce4" > ~/.xsession
 chmod +x ~/.xsession
 
@@ -39,4 +19,6 @@ rm -f /data/data/com.termux/files/usr/tmp/.X11-unix/X1
 sleep 2
 
 vncserver -geometry 1280x720 :1
+
+xrdp-sesman
 xrdp
